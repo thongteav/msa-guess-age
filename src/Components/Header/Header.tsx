d1d7a2f3090aa0c71e38abcdfa452737d9ca21f9
thongteav <thongteav@gmail.com>
2019-07-06 23:49:24 +1200

    added theme toggle between light and dark

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,64 @@
 import * as React from 'react'
+import 'react-toggle/style.css'
 import './Header.css'
 
-export default function Header() {
-    return (
-        <div className="header">
-            <h1 className='headerText'>How Old? 🤔</h1>
-        </div>
-    )
+import { IoIosMoon } from "react-icons/io";
+import { IoIosSunny } from "react-icons/io";
+import Toggle from 'react-toggle'
+
+
+interface IState {
+    checked: boolean
+}
+
+export default class Header extends React.Component<any, IState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            checked: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    public handleChange(event: any) {
+        this.setState({
+            checked: event.target.checked
+        })
+
+        document.body.classList.toggle('nightmode');
+
+        const headerText = document.querySelector('.headerText') as HTMLElement;
+        headerText.classList.toggle('nightmode');
+
+        const dropZone = document.querySelector('.dropZone') as HTMLElement;
+        dropZone.classList.toggle('nightmode');
+
+        const dank = document.querySelector('.dank') as HTMLElement;
+        dank.classList.toggle('nightmode');
+    }
+
+    public render() {
+        return (
+            <div>
+                <div className="right">
+                    <label>
+                        <Toggle
+                            className='nightmode-toggle'
+                            icons={{
+                                checked: <IoIosMoon />,
+                                unchecked: <IoIosSunny />,
+                            }}
+                            onChange={ this.handleChange }/>
+                        <span>Night Mode</span>
+                    </label>
+                </div>
+                <div className="header">
+                    <h1 className='headerText'>How Old? 🤔</h1>
+                </div>
+            </div>
+        )
+    }
+
+
+    
 }
